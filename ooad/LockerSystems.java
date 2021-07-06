58043643b8337a0854183c1ff5c4c62fb4eb31bf
soumatwork <30671512+soumatwork@users.noreply.github.com>
2021-07-06 23:46:06 +1000

    Update LockerSystems.java

--- ooad/LockerSystems.java
+++ ooad/LockerSystems.java
@@ -1,14 +1,76 @@
 class LockerSystems {
 	List<LockerSystem> systems;
+	
+	public LockerSystem findLocker(Shipment shipment) {
+		//find locker system based on location;
+		// you could use QuadTree to store 
+	}
+}
+
+class UsersRepo {
+	Map<String, User> users = new HashMap<>();
+	
+	public User findUser(String registrationId) {
+		return users.get(registrationId);
+	}
 }
 
 class LockerSystem {
 	List<Locker> lockers = new ArrayList<>();
+	Map<String, Locker> usedLocker = new HashMap<>();
+	UsersRepo userRepo;
 	
-	public Locker findLocker(Package pkg) {
+	public LockerSystem(UsersRepo userRepo, int numberOfLockers) {
+		this.userRepo = userRepo;
+		
+		for(int i=0; i < numberOfLockers; i++) {
+			lockers.add(new Locker());
+		}
+	}
+	
+	public boolean findLocker(Package pkg) {
+		if(!userRepo.containsKey(pkg.registrationId)) {
+			return false;
+		}
+		
+		Locker selectedLocker = null;
+		
+		for(Locker locker: lockers) {
+			if(!locker.inUse() && sizeMatched(pkg.size, locker.size)) {
+				selectedLocker = locker;
+			}
+		}
+		
+		if(selectedLocker == null) {
+			return false;
+		}
+		
+		String code = generateCode();
+		usedLocker.put(code, selectedLocker);
+		userRepo.get(pkg.registrationId).notify(code);
 	}
 	
 	public Package openLocker(String code) {
+		if(usedLocker.containsKey(code)) {
+			Locker locker = usedLocker.get(code);
+			return locker.returnPackage();
+		}
+		return null;
+	}
+	
+	private String generateCode() {
+		UUID uuid = UUID.randomUUID();
+		String code = uuid.toString();
+		
+		while(usedLocker.containsKey(code)) {
+			code = UUID.randomUUID().toString();
+		}
+		
+		return code;
+	}
+	
+	private boolean sizeMatched(Size size, Size container) {
+		return container.height > size.height && container.width > size.width && container.length > size.length;
 	}
 }
 
@@ -17,8 +79,18 @@ class Locker {
 	Location location;
 	Package pkg;
 	
+	public boolean inUse() {
+		return pkg != null;
+	}
+	
 	public void setPackage(Package pkg) {
-		
+		this.pkg = pkg;
+	}
+	
+	public Package returnPackage() {
+		Package toReturn = pkg;
+		pkg = null;
+		return toReturn;
 	}
 }
 
@@ -43,6 +115,8 @@ class Order {
 }
 
 class Package {
+	String name;
+	String registrationId;
 	Location location;
 	Size size;
 }
@@ -50,4 +124,9 @@ class Package {
 class User {
 	String name;
 	String address;
+	NotificationMethod notificationMethod;
+	
+	public void notify(String code) {
+		notificationMethod.notify(code);
+	}
 }
