4b3cc3842145d39942f8ff7af99c849462743c25
soumatwork <30671512+soumatwork@users.noreply.github.com>
2021-07-06 21:45:39 +1000

    Update LockerSystems.java

--- ooad/LockerSystems.java
+++ ooad/LockerSystems.java
@@ -1,11 +1,53 @@
-class Locker {
+class LockerSystems {
+	List<LockerSystem> systems;
 }
 
-class Order {
+class LockerSystem {
+	List<Locker> lockers = new ArrayList<>();
+	
+	public Locker findLocker(Package pkg) {
+	}
+	
+	public Package openLocker(String code) {
+	}
+}
+
+class Locker {
+	Size size;
+	Location location;
+	Package pkg;
+	
+	public void setPackage(Package pkg) {
+		
+	}
 }
 
 class Location {
+	int x;
+	int y;
 }
 
-class Shipment {  
+class Size {
+	int width;
+	int height;
+	int length;
+}
+
+class Shipment {
+	Order order;
+	List<Package> packages;
+}
+
+class Order {
+	User user;
+}
+
+class Package {
+	Location location;
+	Size size;
+}
+
+class User {
+	String name;
+	String address;
 }
