7ac7853f5da5ebce07478fff7c1ea10479ec0aba
soumatwork <30671512+soumatwork@users.noreply.github.com>
2021-08-14 18:15:47 +1000

    Update CountingSort.java

--- ooad/CountingSort.java
+++ ooad/CountingSort.java
@@ -3,16 +3,19 @@ class CountingSort {
     int[] workspace = new int[k + 1];
     int[] result = new int[nums.length];
     
+    //count the number of elements including duplicates
     for(int i = 0; i < nums.length; i++) {
       workspace[nums[i]]++;
     }
     
+    //count the overall numbers
     for(int i = 1; i < workspace.length; i++) {
       workspace[i] = workspace[i] + workspace[i - 1];
     }
     
+    //copy the number from original array and decrement the count from workspace
     for(int i = nums.length - 1; i >= 0; i--) {
-      result[workspace[nums[i]]] = nums[i];
+      result[workspace[nums[i]] - 1] = nums[i];
       workspace[nums[i]] --;
     }
     
